4a56a2ac764350e1762c6be173ce6a643ca94b80
João Pedro <jp663687@gmail.com>
2022-08-29 20:09:22 -0300

    commit message

---
+++ hash.java
@@ -0,0 +1,52 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class hash {
+    public static void main(String[] args) {
+
+        Hashtable<String, String> listaTelefonica = new Hashtable<String, String>();
+        Scanner scan = new Scanner(System.in);
+
+        int opcao = 0;
+        String nome ;
+        String telefone ;
+
+        do{
+            System.out.println("");
+            System.out.println("1 - ADICIONAR");
+            System.out.println("2 - BUSCAR");
+            System.out.println("3 - SAIR");
+            System.out.println("");
+
+            opcao = scan.nextInt();
+
+            switch(opcao) {
+                case 1:
+                    System.out.println("Informe o nome: ");
+                    scan.nextLine();
+                    nome = scan.nextLine();
+                    System.out.println("Informe o telefone: ");
+                    telefone = scan.nextLine();
+
+                    listaTelefonica.put(nome, telefone);
+                    break;
+                case 2:
+                    System.out.println("Qual o nome do usuário que você deseja saber o número telefônico");
+                    scan.nextLine();
+                    String nomePesquisa = scan.nextLine();
+                    if(listaTelefonica.get(nomePesquisa) != null) {
+                        System.out.println("O número telefônico é: " + listaTelefonica.get(nomePesquisa));
+                    }else{
+                        System.out.println("Esse nome não existe");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Sistema Finalizado");
+                    break;
+                default:
+                    System.out.println("Não encontrado");
+                    break;
+            }
+        }while(opcao != 3);
+    }
+}
